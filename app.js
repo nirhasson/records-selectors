f6750c279508acc619240fa7c9d4131c4bdd9b8b
nirhasson <47393082+nirhasson@users.noreply.github.com>
2025-02-24 13:53:33 +0200

    last

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
-const response = await fetch('https://wax-riffle.vercel.app/album');
-
 
 const { fetchRandomAlbum, fetchCustomAlbum } = require('./discogs.js');
 
